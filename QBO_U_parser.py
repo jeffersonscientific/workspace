124a5f1c5cdea68905e32f97f92427e955059d88
Mark Yoder <mark.yoder@gmail.com>
2019-09-06 22:40:13 -0700

    again with they dynamic typecodes to QBO_U_parser, but correctly now (i think).

--- QBO_U_parser.py
+++ QBO_U_parser.py
@@ -150,7 +150,7 @@ def copy_U_from_QBO_layer(layer_index=0, fpathname_src='', fpath_dest='', fname_
         if fname_dest is None or fname_dest=='':
             fname_dest = 'U_{}_QBO_k{}.nc'.format(fin.variables['lev_p'][layer_index], layer_index)
         fpathname_dest = os.path.join(fpath_dest, fname_dest)
-        u_type = fin.variables['U'].datatype
+        u_type = fin.variables['U'].typecode()
         #
         os.system('rm {}'.format(fpathname_dest))
         if verbose:
@@ -344,7 +344,7 @@ def copy_U_from_QBO_layer_iosafer(layer_index=0, fpathname_src='', fpath_dest=''
             print('** DEBUG: open output file...')
             n_batches = int(numpy.ceil(n_time/batch_size))
         n_time, n_pev, n_lat, n_lon = numpy.shape(fin.variables['U'])
-        u_type = fin.variables['U'].datatype
+        u_type = fin.variables['U'].typecode()
     #
         with contextlib.closing(Nio.open_file(fpathname_dest, 'c')) as fout:
             #
